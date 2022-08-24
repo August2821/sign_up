a6686bc258c66214deb4638ec19e7d06af820b2c
KMK <89673281+August2821@users.noreply.github.com>
2022-08-24 17:31:47 +0900

    Add files via upload

---
+++ test.php
@@ -0,0 +1,25 @@
+<?php
+$con = mysqli_connect('localhost','root','1234','test');              
+if (mysqli_connect_errno($con)){         
+    echo "DB 연결 실패:" . mysqli_connect_error(); 
+}else{         
+    echo "DB 연결 성공<br />" ;	 
+}
+
+extract($_POST);
+echo $name. '<br />';
+echo $email. '<br />';
+echo $phoneNumber. '<br />';
+echo $password. '<br />';
+echo $passwordCheck. '<br />';
+
+mysqli_query($con,"INSERT INTO joinmember (name,email,phoneNumber,password,passwordCheck) VALUES('$name','$email',$phoneNumber,'$password','$passwordCheck')");
+
+mysqli_close($con);
+
+header("location: data.html");
+
+// $q = "INSERT INTO joinmember(name,email,phoneNumber,password,passwordCheck) VALUES ('홍길동f','as@naver.com','14','kkd','kkd')";
+// $mysqli->query($con, $q);
+// $mysqli->close($con);
+?>
